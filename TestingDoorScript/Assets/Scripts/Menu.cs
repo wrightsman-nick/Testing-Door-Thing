0025bad952f1c4b3d4d081f3171c260aa2dff22b
wrightsman-nick <77906618+wrightsman-nick@users.noreply.github.com>
2021-11-29 22:49:17 -0500

    gonna have a stroke

--- TestingDoorScript/Assets/Scripts/Menu.cs
+++ TestingDoorScript/Assets/Scripts/Menu.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Button m_NewGame, m_Quit;
+
     // Start is called before the first frame update
     void Start()
     {
         m_NewGame.onClick.AddListener(TaskOnClick);
         m_Quit.onClick.AddListener(QuitTask);
+    }
+
+    void TaskOnClick()
+    {
+        return;
     }
 
     void QuitTask()
