aec91c7dbd914d63b03c75e67dc04cc38e38aef2
wrightsman-nick <77906618+wrightsman-nick@users.noreply.github.com>
2021-11-30 02:45:45 -0500

    finalizing controls menu

--- TestingDoorScript/Assets/Scripts/ButtonManager.cs
+++ TestingDoorScript/Assets/Scripts/ButtonManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button m_Menu;
+
     void Start()
     {
-        
+        m_Menu.onClick.AddListener(MenuClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void MenuClick()
     {
-        
+        SceneManager.LoadScene (0);
     }
 }
